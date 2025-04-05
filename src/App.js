d74da8a6895b0f6de23de4ca1aea4e88e13231ae
Tracy Njoroge <tracywangari997@gmail.com>
2025-04-05 12:40:32 +0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import Header from "./Header";
 import Main from "./Main";
+import Footer from "./Footer";
 import './App.css';
+import './body.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
+    <div>
+      <div className="App">
+        <Header />
+        <Main />
+      </div>
+      <div className="body">
+        <Footer />
+      </div>
     </div>
+    
   );
 }
