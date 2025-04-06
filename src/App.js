0c74de8f5c9e4e1016dce245b86ae5d0c52dbaa5
Tracy Njoroge <tracywangari997@gmail.com>
2025-04-06 19:18:10 +0300

    Remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import Header from "./Header";
-import Main from "./Main";
-import Footer from "./Footer";
 import './App.css';
-import './body.css';
 
 export default function App() {
   return (
-    <div>
-      <div className="App">
-        <Header />
-        <Main />
-      </div>
-      <div className="body">
-        <Footer />
-      </div>
-    </div>
-    
+<div>
+  Weather App
+</div>
   );
 }
