07cff62e6300a5a589089ba661ea9e1e6dd860d1
Tracy Njoroge <tracywangari997@gmail.com>
2025-04-07 21:42:35 +0300

    Add the weather app html boilerplate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
       <div className="App">
         <div className="container">
-            <h1>Weather App</h1>
+            <Weather />
             <footer>
               This project was coded by <a href="https://github.com/26TracyNjoroge" target="_blank" rel="noreferrer">Tracy Njoroge</a> is <a href="https://github.com/26TracyNjoroge/weather-react" target="_blank" rel="noreferrer">open-sourced on GitHub </a> and is hosted on <a href="https://shecodes-weather-react.vercel.app/" target="_blank" rel="noreferrer">Vercel</a>
             </footer>
