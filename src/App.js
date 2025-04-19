9ac69d3ff29be1cc854a108d92c4272e4a069c48
Tracy Njoroge <tracywangari997@gmail.com>
2025-04-19 08:02:58 +0300

    Refactor the code and fixed the warnings

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
         <div className="container">
             <Weather defaultCity="Nairobi" />
             <footer>
-              This project was coded by <a href="https://github.com/26TracyNjoroge" target="_blank" rel="noreferrer">Tracy Njoroge</a> is <a href="https://github.com/26TracyNjoroge/weather-react" target="_blank" rel="noreferrer">open-sourced on GitHub </a> and is hosted on <a href="https://shecodes-weather-react.vercel.app/" target="_blank" rel="noreferrer">Vercel</a>
+              This project was coded by <a href="https://github.com/26TracyNjoroge" target="_blank" rel="noopener noreferrer">Tracy Njoroge</a> is <a href="https://github.com/26TracyNjoroge/weather-react" target="_blank" rel=" noopener noreferrer">open-sourced on GitHub </a> and is hosted on <a href="https://shecodes-weather-react.vercel.app/" target="_blank" rel="noopener noreferrer">Vercel</a>
             </footer>
         </div>
       </div>
